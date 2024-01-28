572aa5eb587d7685a62e24b451a97cb106796d1a
Haris Mahmood <haris.mahmood103@gmail.com>
2024-01-28 23:38:48 +0500

    chnagesssss

--- src/assets/URLs.ts
+++ src/assets/URLs.ts
@@ -1,3 +1,5 @@
 // export const URL_Localhost = 'http://localhost:4000/'
-export const URL_Localhost = 'https://benevolent-griffin-d98b34.netlify.app/'
+// export const URL_Localhost = 'https://benevolent-griffin-d98b34.netlify.app/'
+export const URL_Localhost = 'https://marvelous-hamster-651aba.netlify.app/'
+
 
