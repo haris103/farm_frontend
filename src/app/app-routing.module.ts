a901e3630d5fdddd730d2ff260033539ca373c1a
Haris Mahmood <haris.mahmood103@gmail.com>
2024-01-29 00:28:42 +0500

    hahha

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -55,7 +55,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, {useHash: true},{ preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes,{ preloadingStrategy: PreloadAllModules })
   ],
   exports: [RouterModule]
 })
